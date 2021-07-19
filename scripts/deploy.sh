2b77fe4613039bc222a4cdaf89c7bfe617c2ab54
hezhuoyi <zhuoyihe@trip.com>
2021-07-19 10:51:25 +0800

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,6 +22,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:hezhuoyi/blog.git master:gh-pages
+git push -f https://github.com/hezhuoyi/blog.git master:gh-pages
 
 cd -
